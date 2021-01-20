5a17782d0db74b6f657ead7977c19fdaa893a679
Logan Merchant <merchantclogan@gmail.com>
2021-01-20 15:46:21 -0700

    add basic query types/resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,30 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-
+  Query: {
+    getUser: async (parent, { _id }) => {
+      return await User.findOne({
+        _id: id,
+      }).populate("projects");
     },
-    Mutation: {
-
-    }
+    getProjects: async (parent, { _id }) => {
+      if (_id) {
+        return await Project.findOne({
+          _id,
+        });
+      } else {
+        return await Project.find();
+      }
+    },
+    getChapter: async (parent, { projectId }) => {
+      return await Chapter.findOne({
+        _id: projectId,
+      });
+    },
+  },
+  Mutation: {},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,37 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
 type User {
+    firstName: String
+    lastName: String
+    email: String
+    projects: [Project]
+}
+
+type Project {
     
 }
-type Query {
 
+type Chapter {
+
+}
+
+type Votes {
+
+}
+
+type Comment {
+
+}
+
+type Commit {
+
+}
+
+type Query {
+    getUser: User
+    getProjects(name: String, genre: String): [Project]
+    getChapter(_id: ID!: Chapter
 }
 type Mutation {
 
