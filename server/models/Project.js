c39b6b6191a117e00ef41a42d0d7c147dfc6866f
Logan Merchant <merchantclogan@gmail.com>
2021-01-20 17:46:43 -0700

    add title field to project

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,50 +1,58 @@
-const mongoose = require('mongoose');
-const { Schema } = require('mongoose');
-const chapterSchema = require('./Chapter');
-const voteSchema = require('./Vote');
-const dateFormat = require('../utils/dateFormat');
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+const chapterSchema = require("./Chapter");
+const voteSchema = require("./Vote");
+const dateFormat = require("../utils/dateFormat");
 
 const projectSchema = new Schema(
-    {
-        genre: {
-            type: String,
-            required: 'Please Choose A Genre'
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: timestamp => dateFormat(timestamp)
-        },
-        authorName: {
-            type: String,
-            required: true
-        },
-        collaborators: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ],
-        collabsToAddOrDenyList: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ],
-        chapters: [chapterSchema],
-        upvotes: [voteSchema]
+  {
+    title: {
+      type: String,
+      required: "Please Give This Project A Title",
     },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
+    genre: {
+      type: String,
+      required: "Please Choose A Genre",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    authorName: {
+      type: String,
+      required: true,
+    },
+    collaborators: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    collabsToAddOrDenyList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    chapters: [chapterSchema],
+    upvotes: [voteSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
 );
 
-projectSchema.virtual('chapterCount').get(function () {
-    return this.reactions.length;
+projectSchema.virtual("chapterCount").get(function () {
+  return this.reactions.length;
 });
 
-const Project = mongoose.model('Project', projectSchema);
+projectSchema.virtual("upvoteCount").get(function () {
+  return this.upvotes.length;
+});
+
+const Project = mongoose.model("Project", projectSchema);
 
 module.exports = Project;
