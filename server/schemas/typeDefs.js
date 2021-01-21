d1476099474094b488bee8e7a44e89c702bf8c31
Logan Merchant <merchantclogan@gmail.com>
2021-01-20 17:48:42 -0700

    add type definitions for most of the things

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,35 +6,62 @@ type User {
     lastName: String
     email: String
     projects: [Project]
+    collaborations: [Project]
 }
 
 type Project {
-    
+    title: String
+    genre: String
+    createdAt: String
+    authorName: String
+    chapters: [Chapter]
+    collaborators: [User]
+    collabsToAddOrDenyList: [User]
+    upvotes: [Vote]
 }
 
 type Chapter {
-
+    chapterText: String
+    createdAt: String
+    authorName: String
+    collaborators: [User]
+    comments: [Comment]
+    commits: [Commit]
 }
 
-type Votes {
-
+type Vote {
+    userId: ID
 }
 
 type Comment {
-
+    commentText: String
+    createdAt: String
+    username: String
+    reactions: [Reaction]
 }
 
 type Commit {
+    commitText: String
+    commitType: String
+    createdAt: String
+    username: String
+}
 
+type Reaction{
+    reactionBody: String
+    username: String
+    createdAt: String
 }
 
 type Query {
     getUser: User
-    getProjects(name: String, genre: String): [Project]
+    getLoggedInUser: User
+    getProjectsByUpvote: [Project]
+    getProjectsBySearch(title: String, genre: String, authorName: String): Project
     getChapter(_id: ID!): Chapter
 }
 type Mutation {
-
+    testmcgee: User
 }
 `;
 
