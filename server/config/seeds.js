577aff79490fec25078d5c8848ae59caf9bc63c5
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2021-01-21 15:37:48 -0700

    initial seeds have been made, though I'm still working on it.

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,1 +1,68 @@
-//potentially this file will be deleted later
+const { db } = require("../models/User");
+
+const db = require('./connection');
+const { User, Project } = require('../models');
+
+db.once('open', async () => {
+    await Project.deleteMany();
+
+    const projects = await Project.insertMany([
+        {
+            title: 'Test Title',
+            summary: 'This is a test',
+            genre: 'test',
+            authorName: 'TestTester',
+            isPublic: true,
+            chapters: [
+                {
+                    title: 'test chapter',
+                    chapterText: 'lorem ipsum dromem, yadahahahaha',
+                    comments: [
+                        {
+                            commentText: 'This is a comment',
+                            username: 'theotheruser',
+                            reactions: [
+                                {
+                                    reactionBody: 'This is a reaction',
+                                    username: 'TestTester'
+                                }
+                            ]
+                        }
+                    ],
+                    commits: [
+                        {
+                            commitText: 'This is a commit',
+                            commitType: 'comment/criticism',
+                            username: 'theotheruser'
+                        }
+                    ]
+                }
+            ],
+            upvotes: [
+                {
+                    userId: 1
+                }
+            ]
+        }
+    ])
+
+    await User.deleteMany();
+
+    await User.create({
+        _id: 1,
+        username: 'TestTester',
+        email: 'test@test.com',
+        password: 'test12345',
+        projects: [
+            {
+                projects: [projects[0]]
+            }
+        ]
+    })
+
+    await User.create({
+
+    })
+    console.log('I think its all seeded');
+    process.exit();
+});
