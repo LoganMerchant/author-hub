7c09cc35eb7f61cf37cae7d224d98c305afdeb68
Logan Merchant <merchantclogan@gmail.com>
2021-01-21 13:05:25 -0700

    add all initial typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,7 +16,7 @@ const resolvers = {
 
     // Get the loggedin user
     getLoggedInUser: async (parent, args, context) => {
-      if (context) {
+      if (context.user) {
         return await User.findOne({
           _id: context._id,
         })
@@ -50,7 +50,7 @@ const resolvers = {
 
     // Get all of the project info
     getProjectInfo: async (parent, { _id }, context) => {
-      if (context) {
+      if (context.user) {
         return await Project.findOne({ _id })
           .populate("collaborators")
           .populate("collabsToAddOrDenyList")
@@ -61,8 +61,202 @@ const resolvers = {
         "You need to be logged in to view this project."
       );
     },
+
+    // Get a chapter of a book
+    getChapter: async (parent, { bookId, _id }) => {
+      await Project.findOne({
+        _id: bookId,
+      })
+        .populate("chapters")
+        .execPopulate()
+        .then((book) => {
+          book.chapters.forEach((chapter) => chapter._id === _id);
+        });
+    },
   },
-  Mutation: {},
+  Mutation: {
+    // Allows user to login and set their data to the JWT
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials...");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError("Incorrect credentials....");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    // Creates a new account and set the user's data to the JWT
+    addUser: async (parent, args) => {
+      const user = User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    // Creates a new project(book)
+    addProject: async (parent, args, context) => {
+      if (context.user) {
+        return await Project.create(args);
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    editProjectInfo: async (
+      parent,
+      { title, summary, genre, isPublic },
+      context
+    ) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id },
+          { title, summary, genre, isPublic },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Deletes a project
+    deleteProject: async (parent, { _id }, context) => {
+      if (context.user) {
+        return await Project.deleteOne({ _id });
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Accept a collaborator to project
+    acceptCollaborator: async (parent, { projectId, userId }, context) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id: projectId },
+          {
+            $addToSet: { collaborators: userId },
+            $pull: { collabsToAddOrDenyList: userId },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Deny a collaborator
+    denyCollaborator: async (parent, { projectId, userId }, context) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id: projectId },
+          { $pull: { collabsToAddOrDenyList: userId } },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Add chapter
+    addChapter: async (parent, { projectId, ...args }, context) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          {
+            _id: projectId,
+          },
+          { $addToSet: { chapters: args } },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Add comment to a chapter nested within a project (public)
+    addComment: async (
+      parent,
+      { projectId, chapterId, commentText },
+      context
+    ) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id: projectId, "chapters._id": chapterId },
+          {
+            $addToSet: {
+              "chapters.$.comments": {
+                commentText,
+                username: context.user.username,
+              },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+        // MAY WANT TO DO A `Project.findOne().then()` IF THIS FAILS
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Add commit to a chapter nested within a project (private)
+    addCommit: async (
+      parent,
+      { projectId, chapterId, commitText, commitType },
+      context
+    ) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id: projectId, "chapters._id": chapterId },
+          {
+            $addToSet: {
+              "chapters.$.commits": {
+                commitText,
+                commitType,
+                username: context.user.username,
+              },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+
+    // Upvote a project
+    upvoteProject: async (parent, { projectId }, context) => {
+      if (context.user) {
+        const project = await Project.findByIdAndUpdate(
+          { _id: projectId },
+          { $addToSet: { upvotes: context.user._id } },
+          { new: true, runValidators: true }
+        );
+
+        return project;
+      }
+
+      throw new AuthenticationError("You need to be logged in...");
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,68 +1,95 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type User {
-    firstName: String
-    lastName: String
+  type User {
+    username: String
     email: String
     projects: [Project]
     collaborations: [Project]
-}
+  }
 
-type Project {
+  type Project {
     title: String
+    summary: String
     genre: String
     createdAt: String
     authorName: String
+    isPublic: Boolean
     chapters: [Chapter]
     collaborators: [User]
     collabsToAddOrDenyList: [User]
     upvotes: [Vote]
-}
+  }
 
-type Chapter {
+  type Chapter {
+    title: String
     chapterText: String
     createdAt: String
-    authorName: String
+    isPublic: Boolean
     collaborators: [User]
     comments: [Comment]
     commits: [Commit]
-}
+  }
 
-type Vote {
+  type Vote {
     userId: ID
-}
+  }
 
-type Comment {
+  type Comment {
     commentText: String
     createdAt: String
     username: String
     reactions: [Reaction]
-}
+  }
 
-type Commit {
+  type Commit {
     commitText: String
     commitType: String
     createdAt: String
     username: String
-}
+  }
 
-type Reaction{
+  type Reaction {
     reactionBody: String
     username: String
     createdAt: String
-}
+  }
 
-type Query {
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
     getUser: User
     getLoggedInUser: User
     getProjectsByUpvote: [Project]
-    getProjectsBySearch(title: String, genre: String, authorName: String): Project
-    getChapter(_id: ID!): Chapter
-}
-type Mutation {
-    testmcgee: User
-}
+    getProjectsBySearch(
+      title: String
+      genre: String
+      authorName: String
+    ): Project
+    getChapter(bookId: ID!, _id: ID!): Chapter
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addProject(title: String!, genre: String!): Project
+    editProjectInfo(
+      title: String
+      summary: String
+      genre: String
+      isPublic: Boolean
+    ): Project
+    deleteProject(_id: ID): Project
+    acceptCollaborator(projectId: ID!, userId: ID!): Project
+    denyCollaborator(projectId: ID!, userId: ID!): Project
+    addChapter(projectId: ID!, title: String!, chapterText: String!): Project
+    addComment(commentText: String!): Project
+    addCommit(commitText: String!, commitType: String!): Project
+    upvoteProject(userId: ID!): Project
+  }
 `;
 
 module.exports = typeDefs;
