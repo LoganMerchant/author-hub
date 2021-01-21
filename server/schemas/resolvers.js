a0aa01c67b0ff0d07f863058f5552afee9b36c7f
Logan Merchant <merchantclogan@gmail.com>
2021-01-20 17:49:08 -0700

    add initial few resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,65 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User, Project } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    // Get any user by its `_id`
     getUser: async (parent, { _id }) => {
       return await User.findOne({
-        _id: id,
-      }).populate("projects");
+        _id,
+      })
+        .populate("projects")
+        .populate("collaborations")
+        .execPopulate();
     },
-    getProjects: async (parent, { _id }) => {
-      if (_id) {
-        return await Project.findOne({
-          _id,
-        });
-      } else {
-        return await Project.find();
+
+    // Get the loggedin user
+    getLoggedInUser: async (parent, args, context) => {
+      if (context) {
+        return await User.findOne({
+          _id: context._id,
+        })
+          .populate("projects")
+          .populate("collaborations")
+          .execPopulate();
       }
+
+      throw new AuthenticationError(`You're not logged in...`);
     },
-    getChapter: async (parent, { projectId }) => {
-      return await Chapter.findOne({
-        _id: projectId,
-      });
+
+    // Get all of the projects, sorting them by most upvotes
+    getProjectsByUpvote: async () => {
+      return await Project.find({
+        sort: { upvotes: -1 },
+      })
+        .populate("collaborators")
+        .execPopulate();
+    },
+
+    // Get all of the projects that match the searched title, genre or authorName
+    getProjectsBySearch: async (parent, { title, genre, authorName }) => {
+      return await Project.finc({
+        title,
+        genre,
+        authorName,
+      })
+        .populate("collaborators")
+        .execPopulate();
+    },
+
+    // Get all of the project info
+    getProjectInfo: async (parent, { _id }, context) => {
+      if (context) {
+        return await Project.findOne({ _id })
+          .populate("collaborators")
+          .populate("collabsToAddOrDenyList")
+          .execPopulate();
+      }
+
+      throw new AuthenticationError(
+        "You need to be logged in to view this project."
+      );
     },
   },
   Mutation: {},
