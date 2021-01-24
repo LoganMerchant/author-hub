34daab1b35c5e58ed3a760e8f3fff05ae052d740
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2021-01-23 18:24:43 -0700

    edited the helpers to include our models ant the potential things that will need to be stored for offline functionality.

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,15 +1,14 @@
 import React, { createContext, useContext } from "react";
-import { useProjectReducer } from './reducers'; //this will need to be changed
+import { useProjectReducer } from './reducers';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProjectReducer({ //this will need to be changed to match the top
-        currentProject: {},
+    const [state, dispatch] = useProjectReducer({
+        currentProject: {}, //both the current project and current chapter will most likely be called.
         currentChapter: {},
-        projects: [],
-        chapters: []
+        chapters: [] //This will be needed for the table of contents component.
     });
     console.log(state); //when everything is working this should probably be deleted.
     return <Provider value={[state, dispatch]} {...props} />;
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -12,9 +12,10 @@ export function idbPromise(storeName, method, object) {
         let db, tx, store;
         request.onupgradeneeded = function (e) {
             const db = request.result;
-            db.createObjectStore('products', { keyPath: '_id' });
-            db.createObjectStore('categories', { keyPath: '_id' });
-            db.createObjectStore('cart', { keyPath: '_id' });
+            db.createObjectStore('projects', { keyPath: '_id' });
+            db.createObjectStore('chapters', { keyPath: '_id' });
+            db.createObjectStore('comments', { keyPath: '_id' });
+            db.createObjectStore('commits', { keyPath: '_id' });
         };
         request.onerror = function (e) {
             console.log('There was an error')
