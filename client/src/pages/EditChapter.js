e3e71ff80ea548d7ee80aa4cf6bfa4fdfdf5fe0a
Logan Merchant <merchantclogan@gmail.com>
2021-01-26 02:18:50 -0700

    add initial edit chapter page & components

--- client/src/pages/EditChapter.js
+++ client/src/pages/EditChapter.js
@@ -1,34 +1,107 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from '@apollo/react-hooks';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_CHAPTER } from "../utils/actions";
-import { QUERY_CHAPTER } from "../utils/queries";
-import { idbPromise } from "../../utils/helpers";
-import TableOfContents from '../components/TableOfContents';
-import CommitList from '../components/CommitList';
+import TableOfContents from "../components/TableOfContents";
+import CommitForm from "../components/CommitForm";
+import CommitList from "../components/CommitList";
+
 const EditChapter = () => {
+  // Get currentChapter from global store
+  const [state] = useStoreContext();
+  const { currentChapter } = state;
 
-    function commitChanges() {
+  // Setup of local state for keeping track of the chapter's data
+  const [updatedChapter, setUpdatedChapter] = useState(currentChapter);
 
-    }
+  // Function that runs anytime a change is made to the chapter's text or title
+  function handleChange() {
+    const updatedTitle = document.querySelector("#formChapterTitle").value;
+    const updatedText = document.querySelector("#formChapterText").value;
 
-    return (
-        <div>
-            <div className="row">
-                <div className="col-3"><TableOfContents /></div>
-                <div className='col-9'>
-                    <textarea id="chapter-text" name="chapter-text">{currentChapter.chapterText}</textarea>
-                </div>
-            </div>
-            <div id="button-container">
-                <button className="float-center" onClick={commitChanges()}>Commit</button>
-            </div>
-            <div id="commit-container">
-                <CommitList />
-            </div>
-        </div>
-    );
+    setUpdatedChapter({
+      ...updatedChapter,
+      title: updatedTitle,
+      chapterText: updatedText,
+    });
+  }
+
+  // Toggles the isPublic value of local state, which will eventually be used to update the chapter's
+  function toggleIsPublic() {
+    const booleanValue = !updatedChapter.isPublic;
+
+    setUpdatedChapter({
+      ...updatedChapter,
+      isPublic: booleanValue,
+    });
+  }
+
+  // JSX
+  return (
+    <Container fluid>
+      <Row>
+        {/* Table of Contents */}
+        <Col sm={12} md={2}>
+          <TableOfContents />
+        </Col>
+
+        {/* Chapter Edit Form */}
+        <Col sm={12} md={8}>
+          {/* Toggles isPublic */}
+          <Form.Group controlId="formChapterIsPublic">
+            {updatedChapter.isPublic === true ? (
+              <Form.Check
+                type="switch"
+                defaultChecked={updatedChapter.isPublic}
+                label="Published"
+                isValid
+                onClick={toggleIsPublic}
+              />
+            ) : (
+              <Form.Check
+                type="switch"
+                label="Unpublished"
+                isInvalid
+                onClick={toggleIsPublic}
+              />
+            )}
+          </Form.Group>
+
+          {/* Edits chapter's title */}
+          <Form.Group controlId="formChapterTitle">
+            <Form.Label column="lg">Title:</Form.Label>
+            <Form.Control
+              as="textarea"
+              defaultValue={currentChapter.title}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          {/* Edits chapter's text */}
+          <Form.Group controlId="formChapterText">
+            <Form.Label column="lg">Content:</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={25}
+              defaultValue={currentChapter.chapterText}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Col>
+
+        {/* Commit Form, which is passed updatedChapter as a prop */}
+        <Col sm={12} md={2}>
+          <div id="commit-container">
+            <CommitForm updatedChapter={updatedChapter} />
+            <CommitList commits={currentChapter.commits} />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default EditChapter;
