17b58d61025d179fec79b1aa1b144120ec71904f
Logan Merchant <merchantclogan@gmail.com>
2021-01-27 03:46:24 -0700

    add several components for edit project and edit chapter

---
+++ client/src/components/IsPublicToggleButton/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+const IsPublicToggleButton = ({ updatedIsPublic, setUpdatedIsPublic }) => {
+  // Function to flip the boolean held in `updatedIsPublic`
+  function toggle() {
+    setUpdatedIsPublic(!updatedIsPublic);
+  }
+
+  return (
+    <Form.Group controlId="projectIsPublic">
+      {/* If it's public, make sure it's green and checked */}
+      {updatedIsPublic === true ? (
+        <Form.Check
+          type="switch"
+          defaultChecked={updatedIsPublic}
+          label="Published"
+          isValid
+          onClick={toggle}
+        />
+      ) : (
+        <Form.Check
+          type="switch"
+          label="Unpublished"
+          isInvalid
+          onClick={toggle}
+        />
+      )}
+    </Form.Group>
+  );
+};
+
+export default IsPublicToggleButton;
