52cf229ff771083e88baea9a06e11d41d1e5fb06
Logan Merchant <merchantclogan@gmail.com>
2021-01-28 14:06:13 -0700

    commented out useEffect functionality due to memory leaks

--- client/src/components/TableOfContents/index.js
+++ client/src/components/TableOfContents/index.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
 import ListGroup from "react-bootstrap/ListGroup";
 
 import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_CURRENT_CHAPTER } from "../../utils/actions";
+import { QUERY_GET_CHAPTERS } from "../../utils/queries";
+import { UPDATE_CHAPTERS } from "../../utils/actions";
 
 const TableOfContents = () => {
   const [state, dispatch] = useStoreContext();
   const { currentChapter, chapters } = state;
 
-  // Will update the global state's `currentChapter` and redirect to read chapter on click.
-  const handleOnClick = async (chapter) => {
-    await dispatch({
-      type: UPDATE_CURRENT_CHAPTER,
-      currentChapter: chapter,
-    });
-  };
+  // const { loading, data } = useQuery(QUERY_GET_CHAPTERS, {
+  //   variables: { _id: currentProject._id },
+  // });
+
+  // useEffect(() => {
+  //   let mounted = true;
+
+  //   if (data) {
+  //     const project = data?.getChapters;
+  //     const queryChapters = project.chapters;
+
+  //     dispatch({
+  //       type: UPDATE_CHAPTERS,
+  //       chapters: queryChapters,
+  //     });
+  //   }
+
+  //   return () => {
+  //     mounted = false;
+  //   };
+  // }, [data, currentChapter, dispatch]);
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <ListGroup>
@@ -24,26 +44,27 @@ const TableOfContents = () => {
         <h3>Table of Contents</h3>
       </ListGroup.Item>
 
-      {chapters.map((chapter, index) => (
-        <div key={chapter._id}>
-          <Link to={`/editchapter/${chapter._id}`}>
-            {/* If the chapter is public */}
-            <ListGroup.Item action onClick={() => handleOnClick(chapter)}>
-              {/* Highlights the currentChapter */}
-              {currentChapter._id === chapter._id ? (
-                <h4 style={{ color: "#EF5D58" }}>
-                  {index + 1}. {chapter.title}
-                </h4>
-              ) : (
-                <h4>
-                  {index + 1}. {chapter.title}
-                </h4>
-              )}
-              {!chapter.isPublic && <p>Unpublished</p>}
-            </ListGroup.Item>
-          </Link>
-        </div>
-      ))}
+      {chapters &&
+        chapters.map((chapter, index) => (
+          <div key={chapter._id}>
+            <Link to={`/editchapter/${chapter._id}`}>
+              {/* If the chapter is public */}
+              <ListGroup.Item action>
+                {/* Highlights the currentChapter */}
+                {currentChapter._id === chapter._id ? (
+                  <h4 style={{ color: "#EF5D58" }}>
+                    {index + 1}. {chapter.title}
+                  </h4>
+                ) : (
+                  <h4>
+                    {index + 1}. {chapter.title}
+                  </h4>
+                )}
+                {!chapter.isPublic && <p>Unpublished</p>}
+              </ListGroup.Item>
+            </Link>
+          </div>
+        ))}
     </ListGroup>
   );
 };
