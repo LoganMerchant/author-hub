2dcbf1fa23186d2664bfa9a08b284acf2ee96bc3
Logan Merchant <merchantclogan@gmail.com>
2021-01-31 14:54:09 -0700

    updated commit form to handle errors, commit list to include commit type

--- client/src/components/CommitList/index.js
+++ client/src/components/CommitList/index.js
@@ -11,11 +11,35 @@ const CommitList = ({ commits }) => {
           </div>
           <div className="card-body">
             {commits &&
-              commits.map((commit) => (
-                <p className="pill mb-3" key={commit._id}>
-                  {commit.commitText} {"// "}
-                  {commit.username} on {commit.createdAt}
-                </p>
+              commits.map((commit, index) => (
+                <div className="pill mb-3" key={commit._id}>
+                  {commit.commitType === "Suggestion" ? (
+                    <h4
+                      style={{
+                        borderBottom: "dashed",
+                        borderBottomWidth: "3px",
+                        borderBottomColor: "var(--tertiary)",
+                      }}
+                    >
+                      {index + 1}. {commit.commitType}
+                    </h4>
+                  ) : (
+                    <h4
+                      style={{
+                        borderBottom: "dashed",
+                        borderBottomWidth: "3px",
+                        borderBottomColor: "var(--dark)",
+                      }}
+                    >
+                      {index + 1}. {commit.commitType}
+                    </h4>
+                  )}
+
+                  <h5>{commit.commitText}</h5>
+                  <p>
+                    {commit.username} on {commit.createdAt}
+                  </p>
+                </div>
               ))}
           </div>
         </div>
